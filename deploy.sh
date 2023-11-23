deff9bb5e6b6c27d95325d7f71f876151fcd4791
wonder1218 <zhengwen_man@163.com>
2023-11-23 15:24:44 +0800

    fixed:修改bash打包配置

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,22 @@
-#!/usr/bin/env sh
+   #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
-set -e
+   # abort on errors
+   set -e
 
-# 生成静态文件
-npm run build
+   # build
+   npm run docs:build
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
+   # navigate into the build output directory
+   cd .vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.yourwebsite.com' > CNAME
+   # if you are deploying to a custom domain
+   # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+   git init
+   git add -A
+   git commit -m 'deploy'
 
-# 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:wonder1218/wonder1218.github.io.git master
+   # if you are deploying to https://<USERNAME>.github.io
+   git push -f git@github.com:wonder1218/wonder1218.github.io.git master
 
-# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
-
-cd -
+   cd -
